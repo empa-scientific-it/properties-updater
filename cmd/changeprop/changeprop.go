4dd61f15f0bbdffecca777667bd4bd8b308918f9
Simone Baffelli <simone.baffelli@empa.ch>
2023-06-26 15:18:03 +0200

    Added mode handler.

--- cmd/changeprop/changeprop.go
+++ cmd/changeprop/changeprop.go
@@ -3,15 +3,40 @@ package main
 import (
 	dao "empa/basi/properties-updater/pkg/dao"
 	ioutils "empa/basi/properties-updater/pkg/io"
+	"errors"
 	"fmt"
 	"github.com/magiconair/properties"
 	"os"
 )
 
-func UpdateFile(propertyFile *os.File, kv dao.KeyValue) {
+type Mode struct {
+	slug string
+}
+
+var (
+	Replace = Mode{"replace"}
+	Create  = Mode{"create"}
+	Uncomment = Mode{"uncomment"}
+	Comment = Mode{"comment"}
+	Unknown = Mode{"unknown"}
+)
+
+func FromString(s string) (Mode, error) {
+	switch s {
+	case Replace.slug:
+		return Replace, nil
+	case Create.slug:
+		return Create, nil
+	}
+	return Unknown, errors.New("unknown mode: " + s)
+}
+
+func UpdateFile(propertyFile *os.File, kv dao.KeyValue, mode Mode) {
 	p := properties.MustLoadFile(propertyFile.Name(), properties.UTF8)
 	prev, ok, err := p.Set(kv.Key, kv.Value)
-	if ok {
+		
+	fmt.Println(ok)
+	if (ok && mode == Replace) || (!ok && mode == Create) {
 		fmt.Println("Updating property file: " + propertyFile.Name())
 		fmt.Println("Setting property: " + kv.Key + " with value: " + prev + " to value: " + kv.Value)
 		fmt.Println("The file is now:")
@@ -30,16 +55,20 @@ func UpdateFile(propertyFile *os.File, kv dao.KeyValue) {
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	if len(argsWithoutProg) != 3 {
-		fmt.Println("Usage: properties-updater <property-file> <property-name> <property-value>")
+	if len(argsWithoutProg) != 4 {
+		fmt.Println("Usage: properties-updater <property-file> <mode> <property-name> <property-value>")
 		return
 	}
+	fmt.Println(argsWithoutProg)
+
 	propertyFile := argsWithoutProg[0]
-	propertyName := argsWithoutProg[1]
-	propertyValue := argsWithoutProg[2]
+	mode, err := FromString(argsWithoutProg[1])
+	ioutils.HandleError(err)
+	propertyName := argsWithoutProg[2]
+	propertyValue := argsWithoutProg[3]
 	pf, err := os.OpenFile(propertyFile, os.O_WRONLY, 0600)
 	if err == nil {
-		UpdateFile(pf, dao.KeyValue{Key: propertyName, Value: propertyValue})
+		UpdateFile(pf, dao.KeyValue{Key: propertyName, Value: propertyValue}, mode)
 		pf.Sync()
 
 	} else {
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module empa/basi/properties-updater
 
 go 1.20
 
-require github.com/magiconair/properties v1.8.7 // indirect
+require github.com/magiconair/properties v1.8.7
